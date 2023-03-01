5cee4f5ca4009b4324d2ab5a393b70ae62a16887
bassjim <106503203+bassjim@users.noreply.github.com>
2023-03-01 11:33:21 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+  <router-view />
+  <button class="btn btn-primary">1111</button>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/front/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+   這是首頁
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
