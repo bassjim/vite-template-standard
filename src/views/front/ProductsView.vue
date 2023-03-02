6c3e9a709f5669ea6e462977570b223270d1a30a
bassjim <106503203+bassjim@users.noreply.github.com>
2023-03-02 23:45:57 +0800

    add this.produtcts陣列

--- src/views/front/ProductsView.vue
+++ src/views/front/ProductsView.vue
@@ -5,13 +5,14 @@
             <tr v-for="product in products" :key="product.id">
             <td>{{ product.title }}</td>
             <td><img :src="product.imageUrl" width="200" alt=""></td>
-            <td></td>
+            <td><router-link :to="`product/${product.id}`" >查看單一產品</router-link></td>
             </tr>
         </tbody>
     </table>
 </template>
 
 <script>
+import { RouterLink } from 'vue-router'
 const { VITE_URL, VITE_PATH } = import.meta.env
 
 export default {
@@ -20,11 +21,15 @@ export default {
       products: []
     }
   },
+  components: {
+    RouterLink
+  },
   methods: {
     getProducts () {
       this.$http.get(`${VITE_URL}/api/${VITE_PATH}/products/all`)
         .then((res) => {
-          console.log(res)
+          this.products = res.data.products
+          console.log(res.data.products)
         })
     },
     mounted () {
