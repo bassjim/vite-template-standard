60c5a28ef397de7820e798f38fd82b90b0582caa
bassjim <106503203+bassjim@users.noreply.github.com>
2023-03-17 23:01:02 +0800

    add 3/17

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -1,6 +1,155 @@
 <template>
   <div class="home">
-   這是首頁
+    <div class="position-relative">
+      <div class="position-absolute" style="top:0; bottom:0; left:0; right:0; background-image: url(https://images.pexels.com/photos/4822293/pexels-photo-4822293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
+      background-position: center center; opacity: 0.2;"></div>
+      <div class="container d-flex flex-column" style="min-height: 100vh;">
+        <div class="row justify-content-center my-auto">
+          <div class="col-md-4 text-center">
+            <h1>台灣二手路亞交流平台.</h1>
+            <p class="text-muted mb-0">每一個路亞釣手都有大魚夢，快來加入這個大家族吧!</p>
+            <!-- <button class="btn btn-dark rounded-0 mt-6">Lorem ipsum.</button> -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-md-4 mt-md-4">
+          <div class="card border-0 mb-4">
+            <img
+              src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
+              class="card-img-top rounded-0"
+              alt="..."
+            />
+            <div class="card-body text-center">
+              <h4>Lorem ipsum</h4>
+              <div class="d-flex justify-content-between">
+                <p class="card-text text-muted mb-0">
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mt-md-4">
+          <div class="card border-0 mb-4">
+            <img
+              src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
+              class="card-img-top rounded-0"
+              alt="..."
+            />
+            <div class="card-body text-center">
+              <h4>Lorem ipsum</h4>
+              <div class="d-flex justify-content-between">
+                <p class="card-text text-muted mb-0">
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mt-md-4">
+          <div class="card border-0 mb-4">
+            <img
+              src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
+              class="card-img-top rounded-0"
+              alt="..."
+            />
+            <div class="card-body text-center">
+              <h4>Lorem ipsum</h4>
+              <div class="d-flex justify-content-between">
+                <p class="card-text text-muted mb-0">
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-light mt-7">
+      <div class="container">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="row justify-content-center py-7">
+                <div class="col-md-6 text-center">
+                  <h3>Lorem ipsum.</h3>
+                  <p class="my-5">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.”</p>
+                  <p><small>—Lorem ipsum dolor sit amet.—</small></p>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="row justify-content-center py-7">
+                <div class="col-md-6 text-center">
+                  <h3>Lorem ipsum.</h3>
+                  <p class="my-5">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.”</p>
+                  <p><small>—Lorem ipsum dolor sit amet.—</small></p>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="row justify-content-center py-7">
+                <div class="col-md-6 text-center">
+                  <h3>Lorem ipsum.</h3>
+                  <p class="my-5">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.”</p>
+                  <p><small>—Lorem ipsum dolor sit amet.—</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls"  role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="container my-7">
+      <div class="row">
+        <div class="col-md-6">
+          <img src="https://images.unsplash.com/photo-1490312278390-ab64016e0aa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="" class="img-fluid">
+        </div>
+        <div class="col-md-4 m-auto text-center">
+          <h4 class="mt-4">Lorem ipsum</h4>
+          <p class="text-muted">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.</p>
+        </div>
+      </div>
+      <div class="row flex-row-reverse justify-content-between mt-4">
+        <div class="col-md-6">
+          <img src="https://images.unsplash.com/photo-1490312278390-ab64016e0aa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="" class="img-fluid">
+        </div>
+        <div class="col-md-4 m-auto text-center">
+          <h4 class="mt-4">Lorem ipsum</h4>
+          <p class="text-muted">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.</p>
+        </div>
+      </div>
+    </div>
+    <div class="bg-light py-4">
+      <div class="container">
+        <div class="d-flex flex-column flex-md-row justify-content-between align-items-md-center align-items-start">
+          <p class="mb-0 fw-bold">Lorem ipsum dolor sit amet.</p>
+          <div class="input-group w-md-50 mt-md-0 mt-3">
+            <input type="text" class="form-control rounded-0" placeholder="" />
+            <div class="input-group-append">
+              <button class="btn btn-dark rounded-0" type="button" id="search">
+                Lorem ipsum
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
--- src/views/front/ProductsView.vue
+++ src/views/front/ProductsView.vue
@@ -1,18 +1,59 @@
 <template>
 <VueLoading :active="isLoading"></VueLoading>
-  <div>產品列表</div>
-  <table class="table">
-    <tbody>
-      <tr v-for="product in products" :key="product.id">
-        <td>{{ product.title }}</td>
-        <td><img :src="product.imageUrl" width="200" alt="" /></td>
-        <td><router-link :to="`product/${product.id}`" class="btn btn-secondary">查看單一產品</router-link>
-          <button type="button" class="btn btn-primary"
-          @click="addTOCart(product.id)"><i class="bi bi-cart-check"></i>加入購物車</button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+<div class="d-flex mt-5 justify-content-around container">
+  <ul class="nav nav-pills mb-3 flex-column" id="pills-tab" role="tablist">
+      <li class="nav-item" role="presentation">
+        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+      </li>
+      <li class="nav-item" role="presentation">
+        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+      </li>
+      <li class="nav-item" role="presentation">
+        <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+      </li>
+      <li class="nav-item" role="presentation"  v-for="item in categories" :key="item">
+        <router-link
+                    :to="{
+                      query: { category: item, page: 1 },
+                    }"
+                    :key="item"
+                  >
+                  <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">{{ item }}</button>
+          </router-link>
+      </li>
+    </ul>
+  <div class="d-flex row row-cols-2 row-cols-md-3 flex-wrap col-md-8"  v-if="filteredProducts[filteredProducts.length - 1]">
+          <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab"  v-for="product in filteredProducts"
+                :key="product.id">
+                <RouterLink
+                  :to="`/product/${product.id}`"
+                  class="card overflow-hidden"
+                >
+                <div class="card mb-3 d-flex" >
+                <div class="row g-0 ">
+                  <div class="mx-auto">
+                    <img :src="product.imageUrl" class="img-fluid rounded-star" alt="...">
+                  </div>
+                  <div class="">
+                    <div class="card-body">
+                      <h5 class="card-title">{{ product.title }}</h5>
+                      <p class="card-text">商品描述：{{product.description}}</p>
+                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                    <div class="card-footer bg-transparent border-success d-flex justify-content-evenly">
+                      <router-link :to="`product/${product.id}`" class="btn btn-secondary">查看單一產品</router-link>
+                        <button type="button" class="btn btn-primary"
+                        @click="addTOCart(product.id)"><i class="bi bi-cart4"></i>加入購物車</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+                </RouterLink>
+
+          <!-- <td><img :src="product.imageUrl" width="200" alt="" /></td> -->
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -24,7 +65,9 @@ export default {
   data () {
     return {
       products: [],
-      isLoading: false
+      filteredProducts: [],
+      isLoading: false,
+      categories: []
     }
   },
   components: {
@@ -37,6 +80,27 @@ export default {
         .then((res) => {
           this.products = res.data.products
         })
+    },
+    filterProducts (page = 1) {
+      let url = `${VITE_URL}/api/${VITE_PATH}/products?page=${page}`
+      const category = this.$route.query.category
+      if (category && category !== '全部') {
+        url = `${VITE_URL}/api/${VITE_PATH}/products?page=${page}&category=${category}`
+      }
+      this.$http
+        .get(url)
+        .then((res) => {
+          this.filteredProducts = res.data.products
+          this.pagination = res.data.pagination
+          this.$router.push({ path: 'products', query: { category, page } })
+        })
+        .catch((err) => console.dir(err))
+    },
+    getCategories () {
+      const categories = [
+        ...new Set(this.products.map((item) => item.category))
+      ]
+      this.categories = categories
     },
     addTOCart (id) {
       const data = {
@@ -60,7 +124,19 @@ export default {
   },
   mounted () {
     this.getProducts()
+    this.filterProducts()
     this.$showLoading()
+  },
+
+  watch: {
+    $route: {
+      handler (val) {
+        if (val.name === 'products') {
+          this.filterProducts(this.$route.query.page)
+        }
+      },
+      deep: true
+    }
   }
 }
 </script>
